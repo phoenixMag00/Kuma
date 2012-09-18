deb98bcc509bc6638a95a2152335de2126b31d1c
Will Rees <rees.will@gmail.com>
2012-09-18 13:02:07 -0400

    Way Better 404 Page
    Gives users the same options as the JiJu 404 page. Also has a cool pic
    of a confused dog!

--- 404.php
+++ 404.php
@@ -8,15 +8,36 @@
 
 		<div class="page">
     	
-			<h2>Oh, this is embarrassing...(404 Page Not Found)</h2>
+			<img src="<?php bloginfo('template_directory'); ?>/library/images/404-fun-image-300-300.jpg" class="alignright" alt="Image for 404 Page" />
+			
+				<h2>404 - Page Not Found</h2>
+			
+					<p>Don't throw in the towel just yet! Troubleshooting tips are right below!</p>
     		
-    			<p>These aren't the droids you're looking for...check the url for errors and try refreshing.</p>
+						<form method="get" action="http://search1.gmu.edu/search">
+							
+							<ul>
+								<li id="search-line">Try
+									<label>searching the Mason Web: 
+									<input name="q" type="text" class="text" maxlength="600"></label> 
+											</li>
+								<li> Check the <a href="http://www.gmu.edu/mlnavbar/masonaz/">Mason A-Z Directory</a>.</li>
+								<li> If you typed the URL yourself, please make sure that the spelling is correct.</li>
+								<li> If you clicked on a link to get here, there may be a problem with the link.</li>
+							</ul>
+							<p class="webmaster">If you still need assistance, contact the <a href="mailto:webmaster@gmu.edu">webmaster</a>.</p>
+							<input type="hidden" name="site" value="mason_test">
+							<input type="hidden" name="client" value="mason_test">
+							<input type="hidden" name="proxystylesheet" value="mason_test">
+							<input type="hidden" name="output" value="xml_no_dtd">
+						
+						</form>
+
     		
 		</div>
 
 	</div>
 	
-
 	</div>
 
 </div>
