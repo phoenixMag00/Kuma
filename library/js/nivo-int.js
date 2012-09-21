d3fc0cb894f1fee52434e4a9e0cb0021d8b1e893
Will Rees <rees.will@gmail.com>
2012-09-21 13:48:27 -0400

    Slider Updates
    Removed all transitions except fade. Kepp it simple, keep it clean.

--- library/js/nivo-int.js
+++ library/js/nivo-int.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
     $('#slider').nivoSlider({
+		effect: 'fade',
 		animSpeed: 700, // Slide transition speed
 		pauseTime: 5000 // How long each slide will show	
     });
