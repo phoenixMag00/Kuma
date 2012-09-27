bfde1a5b2c74ad8e1e458b37edcbf589d1faa819
Will Rees <rees.will@gmail.com>
2012-09-27 11:19:42 -0400

    Slides Appear 3 Second Longer
    Slides Appear 3 Second Longer

--- library/js/nivo-int.js
+++ library/js/nivo-int.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($) {
     $('#slider').nivoSlider({
 		effect: 'fade',
 		animSpeed: 700, // Slide transition speed
-		pauseTime: 5000 // How long each slide will show	
+		pauseTime: 8000 // How long each slide will show	
     });
 });
