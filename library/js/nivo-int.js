4d872b9f6538bc8f05375401af454012727029d5
Will Rees <rees.will@gmail.com>
2012-11-07 14:33:02 -0500

    Minor Updates to Swip Homepage
    Minor Updates to Swip Homepage

--- library/js/nivo-int.js
+++ library/js/nivo-int.js
@@ -1,21 +1,31 @@
 jQuery(document).ready(function($) {
-    $('#slider').nivoSlider({
+
+	$('#slider').nivoSlider({
 		effect: 'fade',
 		animSpeed: 250, // Slide transition speed
 		pauseTime: 9000 // How long each slide will show	
     });
     
-    $('#slider').bind( 'swipeleft', function( e ) {
-		$('a.nivo-nextNav').trigger('click');
-		e.stopImmediatePropagation();
-		return false; } 	
-	);  
+    	//Below is Support for Swiping the Homepage Feature (using custom complied version of jQuery Mobile v1.2)
+    
+    		//First, Remove Hover Next and Previous Arrows on Touch Devices. Don't need them really.
+    			if(jQuery.support.touch){
+	    			$('a.nivo-nextNav').css('visibility', 'hidden');
+	    			$('a.nivo-prevNav').css('visibility', 'hidden');
+	    		}
+    
+	    		//Then Finish Up swipeleft and swiperight
+	    			$('#slider').bind( 'swipeleft', function( e ) {
+		    			$('a.nivo-nextNav').trigger('click');
+		    			e.stopImmediatePropagation();
+		    			return false; } 	
+		    		);  
 
-	$('#slider').bind( 'swiperight', function( e ) {
-		$('a.nivo-prevNav').trigger('click');
-		e.stopImmediatePropagation();
-		return false; } 
-	
-	); 
+		    		$('#slider').bind( 'swiperight', function( e ) {
+			    		$('a.nivo-prevNav').trigger('click');
+			    		e.stopImmediatePropagation();
+			    		return false; } 
+		
+			    	); 
 
 });
