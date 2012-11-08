f10a555be90afaed3490292e18e7e2360db90471
Will Rees <rees.will@gmail.com>
2012-11-08 16:42:49 -0500

    Disable Automated Slide Advancement when jQuery Mobile detects a touch screen

--- library/js/nivo-int.js
+++ library/js/nivo-int.js
@@ -1,18 +1,32 @@
 jQuery(document).ready(function($) {
 
-	$('#slider').nivoSlider({
-		effect: 'fade',
-		animSpeed: 250, // Slide transition speed
-		pauseTime: 9000 // How long each slide will show	
-    });
+	//Below is Support for Swiping the Homepage Feature (using custom complied version of jQuery Mobile v1.2)
     
-    	//Below is Support for Swiping the Homepage Feature (using custom complied version of jQuery Mobile v1.2)
+    	//First, Remove Hover Next and Previous Arrows on Touch Devices. Don't need them really.
+    	
+    		if(jQuery.support.touch){
+	    		
+		    		$('#slider').nivoSlider({
+			    		manualAdvance: true,
+			    		effect: 'fade',
+			    		animSpeed: 250, // Slide transition speed
+			    		pauseTime: 9000 // How long each slide will show
+			    	});
+		    		
+		    		$('a.nivo-nextNav').css('visibility', 'hidden');
+		    		$('a.nivo-prevNav').css('visibility', 'hidden');
+	    		
+	    	}
+	    		
+	    	else { 
+	    	
+		    		$('#slider').nivoSlider({
+			    		effect: 'fade',
+			    		animSpeed: 250, // Slide transition speed
+			    		pauseTime: 9000 // How long each slide will show	
+			    	});
     
-    		//First, Remove Hover Next and Previous Arrows on Touch Devices. Don't need them really.
-    			if(jQuery.support.touch){
-	    			$('a.nivo-nextNav').css('visibility', 'hidden');
-	    			$('a.nivo-prevNav').css('visibility', 'hidden');
-	    		}
+		    	}
     
 	    		//Then Finish Up swipeleft and swiperight
 	    			$('#slider').bind( 'swipeleft', function( e ) {
