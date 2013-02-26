a145e4c62771f18e732291a1413c1b5eda81d058
Will Rees <rees.will@gmail.com>
2013-02-26 10:53:34 -0500

    More touch ups on the DropDowns
    Helps display arrows to indicate which parent links dropdown. No lag
    now! Arrows will be available in the 1.1.3 release.

--- library/js/kuma.js
+++ library/js/kuma.js
@@ -7,21 +7,6 @@ jQuery(document).ready(function() {
 	
 });
 
-// Make Dropdowns keyboard accessible (Using Superfish Plugin)
-function the_superfish() {
-	jQuery('.menu > ul').superfish({
-		delay:       1, 		// one second delay on mouseout
-		speed:       1,			// faster animation speed
-		autoArrows:  true,		// disable generation of arrow mark-up
-		dropShadows: false		// disable drop shadows
-	});
-}
-
-jQuery(document).ready(function() {
-	jQuery.noConflict();
-	the_superfish();
-});
-
 //Clear Top Search Field when user focuses
 function clearValue(field) {
 	if (field.defaultValue == field.value) field.value = "";
---
+++ library/js/superfish-init.js
@@ -0,0 +1,8 @@
+(function($) {
+	$(".menu > ul").superfish({
+		delay:       1, 		// one second delay on mouseout
+		speed:       1,			// faster animation speed
+		autoArrows:  true,		// disable generation of arrow mark-up
+		dropShadows: false		// disable drop shadows
+	});
+})(jQuery);
